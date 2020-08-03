f4ac4824b06c419b5359c9782b4128a4324f9d8e
JakeRe <jake.reilly.20@gmail.com>
2020-08-03 09:45:48 -0500

    Added Animations for the Game's Menus

---
+++ UnityProject/Soviet Warmonger/Assets/Animations/LogoController/DisableSprite.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisableSprite : MonoBehaviour
+{
+
+    [SerializeField]
+    private Image logoImage;
+
+    [SerializeField]
+    private Image studioLogo;
+
+    [SerializeField]
+    private Canvas logoCanvas;
+
+   
+    [SerializeField]
+    private Canvas mainMenuCanvas;
+
+    public void Awake()
+    {
+
+        mainMenuCanvas.gameObject.SetActive(false);
+    }
+
+    private void ChangeCanvas()
+    {
+        logoCanvas.gameObject.SetActive(false);
+        mainMenuCanvas.gameObject.SetActive(true);
+
+    }
+   private void destroySprite()
+    {
+        Destroy(logoImage);
+    }
+
+}
---
+++ UnityProject/Soviet Warmonger/Assets/Scripts/CheckForCanvas.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckForCanvas : MonoBehaviour
+{
+    [SerializeField]
+    private Canvas logoCanvas;
+
+    [SerializeField]
+    private Animator logoAnim;
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        checkForCanvas();
+    }
+
+    private void checkForCanvas()
+    {
+        if(logoCanvas.gameObject.activeInHierarchy == true)
+        {
+            logoAnim.SetBool("Active", false);
+        }
+        if(logoCanvas.gameObject.activeInHierarchy == false)
+        {
+            logoAnim.SetBool("Active", true);
+        }
+
+    }
+
+}
