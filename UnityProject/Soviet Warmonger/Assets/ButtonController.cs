fa075e2d3f62e04cd9fae638d53b620263ee81d3
JakeRe <jake.reilly.20@gmail.com>
2020-08-03 14:28:17 -0500

    Implemented MainMenu Buttons and Credit's Scene

---
+++ UnityProject/Soviet Warmonger/Assets/ButtonController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : MonoBehaviour
+{
+    public void loadCredits()
+    {
+        SceneManager.LoadScene("CreditsMenu");
+    }
+
+    public void closeGame()
+    {
+        Application.Quit();
+
+    }
+
+    public void loadLevel()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void returnToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
--- UnityProject/Soviet Warmonger/Assets/Scripts/CheckForCanvas.cs
+++ UnityProject/Soviet Warmonger/Assets/Scripts/CheckForCanvas.cs
@@ -6,21 +6,23 @@ using UnityEngine.UI;
 public class CheckForCanvas : MonoBehaviour
 {
     [SerializeField]
+    [Tooltip ("Canvas Containing the Logo Game Objects")]
     private Canvas logoCanvas;
 
     [SerializeField]
+    [Tooltip ("Animator for the Logo Game Object")]
     private Animator logoAnim;
-
-    private void Awake()
-    {
-        
-    }
+    
+    [SerializeField]
+    [Tooltip ("Animator for the ButtonGroup Game Object")]
+    private Animator buttonAnim;
 
     private void Update()
     {
         checkForCanvas();
     }
 
+    //Method that checks to see if the canvas for the Different menu objects is active or not.
     private void checkForCanvas()
     {
         if(logoCanvas.gameObject.activeInHierarchy == true)
@@ -32,6 +34,12 @@ public class CheckForCanvas : MonoBehaviour
             logoAnim.SetBool("Active", true);
         }
 
+    }
+
+    //Method that activates the animation for the button group.
+    private void buttonInFrame()
+    {
+        buttonAnim.SetTrigger("LogoIsStill");
     }
 
 }
